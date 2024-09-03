22801ced5ffbdfa6deffc91f7655016dadd024d0
woogi-on_and-on <jgw987@gmail.com>
2024-09-03 16:14:42 +0900

    fix 쿠키 console.log

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -30,6 +30,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   useEffect(() => {
     const checkAuth = () => {
       const AccessToken = Cookies.get('AccessToken');
+      console.log(AccessToken);
       if (AccessToken) {
         setIsLoggedIn(true);
         setToken(AccessToken);
