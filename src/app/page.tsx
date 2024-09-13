854ccb4af0cfa617a0d2b03c6741a7fde69a327c
woogi-on_and-on <jgw987@gmail.com>
2024-09-13 18:10:23 +0900

    feat [public] favicon 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import Main from '../../components/main';
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Main />
     </div>
   );
